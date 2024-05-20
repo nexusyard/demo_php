e368c57667382e351300c261c99f3d12f0a887c0
nexusyard <nexusyard@gmail.com>
2024-05-20 13:56:31 +0530

    init

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo PHP</title>
+</head>
+<body>
+    <div>
+        <h1>Ash webmaster blog...</h1>
+    </div>
+    <div>
+    <p>git hub process</p>
+    <pre>
+    echo "# demo_php" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin git@github.com:nexusyard/demo_php.git
+    git push -u origin main
+    </pre>
+    </div>
+    <div>
+        <p>copyright &copy; || 2024</p>
+    </div>
+</body>
+</html>
